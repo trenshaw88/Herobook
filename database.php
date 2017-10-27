50984dd6f16c699207e743840cd1e2486d654c7b
trenshaw88 <trenshaw88@twc.com>
2017-10-27 11:29:08 -0400

    Updated database.php

--- database.php
+++ database.php
@@ -2,10 +2,27 @@
 
   function getDb() {
 
-    $db = pg_connect("host=localhost port=5432 dbname=herobook user=hero password=herosuper");
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/.env')) {
+      require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+      $dotenv = new Dotenv\Dotenv($_SERVER['DOCUMENT_ROOT']);
+      $dotenv->load();
+    }
 
+    $url = parse_url(getenv("DATABASE_URL"));
+
+    $db_port = $url['port'];
+    $db_host = $url['host'];
+    $db_user = $url['user'];
+    $db_pass = $url['pass'];
+    $db_name = substr($url['path'], 1);
+
+    $db = pg_connect(
+      "host=" . $db_host .
+      " port=" . $db_port .
+      " dbname=" . $db_name .
+      " user=" . $db_user .
+      " password=" . $db_pass);
     return $db;
-
   }
 
 ?>
